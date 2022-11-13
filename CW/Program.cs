e1b22b8bb5c54302c1c7d179dc774f048fe94b10
SsYuRb <113142100+SsYuRb@users.noreply.github.com>
2022-11-14 01:14:22 +0300

    add FillArray

---
+++ CW/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string [] arrayCharsAndDigits = new string [new Random().Next(2,7)]; 
+
+string [] FillArray (string [] array) 
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        char [] CharsArray = new char [new Random().Next(1,5)];
+        for (int k = 0; k < CharsArray.Length; k++)
+        {
+            CharsArray[k] = Convert.ToChar(new Random().Next(33, 123));
+        }
+        string someString = new string (CharsArray);
+        array[i] = someString;
+    }
+    return array;
+}
+FillArray(arrayCharsAndDigits);
